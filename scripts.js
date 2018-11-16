13135d933717cf9505a0e9068ec40e6abeaaec65
rdm100 <robertode_marco@hotmail.com>
2018-11-16 22:41:46 +0000

    Added red effect function which adds a red filter to the photo.

--- scripts.js
+++ scripts.js
@@ -24,7 +24,14 @@ function paintToCanvas(){
 	canvas.height = height;
 
 	return setInterval(() => {
-		ctx.drawImage(video, 0, 0, width, height)
+		ctx.drawImage(video, 0, 0, width, height);
+		// take the pixels out
+		let pixels = ctx.getImageData(0, 0, width, height);
+		// console.log(pixels);
+		// change the values
+		pixels = redEffect(pixels);
+		// put them back
+		ctx.putImageData(pixels, 0, 0);
 	}, 16);
 }
 
@@ -42,6 +49,15 @@ function takePhoto(){
 	// link.textContent = 'Download image';
 	link.innerHTML =  `<img src="${data}" alt="Handsome man" />`
 	strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels){
+	for(var i = 0; i < pixels.data.length; i += 4){
+		pixels.data[i+0] = pixels.data[i + 0] + 100; //red
+		pixels.data[i+1] = pixels.data[i + 1] - 50; //green
+		pixels.data[i+2] = pixels.data[i + 2] * 0.5; //blue
+	}
+	return pixels;
 }
 
 getVideo();
