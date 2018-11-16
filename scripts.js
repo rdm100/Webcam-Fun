7e434845c7b9f3bfd3504165e596d1d4dabce10c
rdm100 <robertode_marco@hotmail.com>
2018-11-16 21:26:13 +0000

    Added paint to canvas function, take photo function and added event listener to call paint to canvas function.

--- scripts.js
+++ scripts.js
@@ -16,4 +16,23 @@ function getVideo(){
 	})
 }
 
+function paintToCanvas(){
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	console.log(width, height);
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height)
+	}, 16);
+}
+
+function takePhoto(){
+	snap.currentTime = 0;
+	snap.play();
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
