092a7da5d22240b9ab4d233c1f17f177047e6f9a
rdm100 <robertode_marco@hotmail.com>
2018-11-16 23:38:56 +0000

    Added green screen function.

--- scripts.js
+++ scripts.js
@@ -30,8 +30,12 @@ function paintToCanvas(){
 		// console.log(pixels);
 		// change the values
 		// pixels = redEffect(pixels);
-		pixels = rgbSplit(pixels);
-		ctx.globalAlpha = 0.8;
+
+		// pixels = rgbSplit(pixels);
+		// ctx.globalAlpha = 0.8;
+
+		pixels = greenScreen(pixels);
+
 		// put them back
 		ctx.putImageData(pixels, 0, 0);
 	}, 16);
@@ -67,6 +71,32 @@ function rgbSplit(pixels){
 		pixels.data[i-150] = pixels.data[i + 0]; //red
 		pixels.data[i+500] = pixels.data[i + 1]; //green
 		pixels.data[i-550] = pixels.data[i + 2]; //blue
+	}
+	return pixels;
+}
+
+function greenScreen(pixels){
+	const levels = {};
+
+	document.querySelectorAll('.rgb input').forEach(input => {
+		levels[input.name] = input.value;
+	});
+
+	for(var i = 0; i < pixels.data.length; i += 4){
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		alpha = pixels.data[i + 1];
+
+		if(red >= levels.rmin
+		    && green >= levels.gmin
+		    && blue >= levels.bmin
+		    && red <= levels.rmax
+		    && green <= levels.gmax
+		    && blue <= levels.rmax
+			){
+			pixels.data[i + 3] = 0;
+		}
 	}
 	return pixels;
 }
